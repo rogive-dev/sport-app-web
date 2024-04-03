516d5616730273863f7258bbb5356117a1f4f773
Ivan Rodriguez <ierodriguezgo@gmail.com>
2024-04-02 22:53:08 -0500

    Add scaffolding

---
+++ src/layouts/dashboard/config.js
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import SvgIcon from '@mui/material/SvgIcon';
+
+import HomeSmileIcon from 'src/icons/untitled-ui/duocolor/home-smile';
+import { tokens } from 'src/locales/tokens';
+import { paths } from 'src/paths';
+
+export const useSections = () => {
+  const { t } = useTranslation();
+
+  return useMemo(() => {
+    return [
+      {
+        items: [
+          {
+            title: t(tokens.nav.registry),
+            path: paths.dashboard.index,
+            icon: (
+              <SvgIcon fontSize="small">
+                <HomeSmileIcon />
+              </SvgIcon>
+            ),
+          },
+        ],
+      }
+    ];
+  }, [t]);
+};
