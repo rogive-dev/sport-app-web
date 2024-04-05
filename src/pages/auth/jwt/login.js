1dcc8b3bc3419fefdc1d6933e5dcf8352fc99c57
Ivan Rodriguez <ierodriguezgo@gmail.com>
2024-04-04 22:09:52 -0500

    (MP-9) Create Login

--- src/pages/auth/jwt/login.js
+++ src/pages/auth/jwt/login.js
@@ -1,15 +1,13 @@
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
-import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
+import Box from '@mui/material/Box';
 import FormHelperText from '@mui/material/FormHelperText';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 
 import { RouterLink } from 'src/components/router-link';
 import { Seo } from 'src/components/seo';
@@ -22,7 +20,7 @@ import { useRouter } from 'src/hooks/use-router';
 import { useSearchParams } from 'src/hooks/use-search-params';
 import { Layout as AuthLayout } from 'src/layouts/auth/classic-layout';
 import { paths } from 'src/paths';
-import { AuthIssuer } from 'src/sections/auth/auth-issuer';
+import { TenantSwitch } from 'src/layouts/dashboard/tenant-switch';
 import { Issuer } from 'src/utils/auth';
 
 const initialValues = {
@@ -50,7 +48,7 @@ const Page = () => {
         await signIn(values.email, values.password);
 
         if (isMounted()) {
-          router.push(returnTo || paths.dashboard.index);
+          router.push(paths.dashboard.registry);
         }
       } catch (err) {
         console.error(err);
@@ -70,88 +68,93 @@ const Page = () => {
     <>
       <Seo title="Login" />
       <div>
-        <Card elevation={16}>
-          <CardHeader
-            subheader={
-              <Typography
-                color="text.secondary"
-                variant="body2"
+        <Stack
+        >
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            direction="row"
+            sx={{ p: 3 }}
+          >
+            <CardMedia
+                image="/assets/logos/logo-sportapp.svg"
+                sx={{ height: 122, width: 409, mb: 6 }}
+              />
+          </Stack>
+          <form
+            noValidate
+            onSubmit={formik.handleSubmit}
+          >
+            <Stack spacing={3}>
+              <TextField
+                autoFocus
+                error={!!(formik.touched.email && formik.errors.email)}
+                fullWidth
+                helperText={formik.touched.email && formik.errors.email}
+                label="Correo Electrónico"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="email"
+                value={formik.values.email}
+              />
+              <TextField
+                error={!!(formik.touched.password && formik.errors.password)}
+                fullWidth
+                helperText={formik.touched.password && formik.errors.password}
+                label="Contraseña"
+                name="password"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="password"
+                value={formik.values.password}
+              />
+            </Stack>
+            {formik.errors.submit && (
+              <FormHelperText
+                error
+                sx={{ mt: 3 }}
               >
-                Don&apos;t have an account? &nbsp;
-                <Link
-                  component={RouterLink}
-                  href={paths.auth.jwt.register}
-                  underline="hover"
-                  variant="subtitle2"
-                >
-                  Register
-                </Link>
-              </Typography>
-            }
-            sx={{ pb: 0 }}
-            title="Log in"
-          />
-          <CardContent>
-            <form
-              noValidate
-              onSubmit={formik.handleSubmit}
+                {formik.errors.submit}
+              </FormHelperText>
+            )}
+            <Stack
+              alignItems="center"
+              justifyContent="center"
+              direction="row"
+              sx={{ p: 3 }}
             >
-              <Stack spacing={3}>
-                <TextField
-                  autoFocus
-                  error={!!(formik.touched.email && formik.errors.email)}
-                  fullWidth
-                  helperText={formik.touched.email && formik.errors.email}
-                  label="Email Address"
-                  name="email"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="email"
-                  value={formik.values.email}
-                />
-                <TextField
-                  error={!!(formik.touched.password && formik.errors.password)}
-                  fullWidth
-                  helperText={formik.touched.password && formik.errors.password}
-                  label="Password"
-                  name="password"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="password"
-                  value={formik.values.password}
-                />
-              </Stack>
-              {formik.errors.submit && (
-                <FormHelperText
-                  error
-                  sx={{ mt: 3 }}
-                >
-                  {formik.errors.submit}
-                </FormHelperText>
-              )}
               <Button
                 disabled={formik.isSubmitting}
-                fullWidth
                 size="large"
-                sx={{ mt: 2 }}
                 type="submit"
                 variant="contained"
               >
-                Log In
+                Iniciar Sesión
               </Button>
-            </form>
-          </CardContent>
-        </Card>
+            </Stack>
+          </form>
+        </Stack>
         <Stack
+          alignItems="center"
+          justifyContent="center"
           spacing={3}
           sx={{ mt: 3 }}
         >
-          <Alert severity="error">
-            <div>
-              You can use <b>demo@devias.io</b> and password <b>Password123!</b>
-            </div>
-          </Alert>
-          <AuthIssuer issuer={issuer} />
+          <Typography
+            color="text.secondary"
+            variant="body2"
+          >
+            ¿No tienes una cuenta aún? &nbsp;
+            <Link
+              component={RouterLink}
+              href={paths.auth.jwt.register}
+              underline="hover"
+              variant="subtitle2"
+            >
+              Registrate
+            </Link>
+          </Typography>
         </Stack>
       </div>
     </>
