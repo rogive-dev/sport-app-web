f60bf61c47e92f3f21786eb38f59aa10de1d95f3
rogive <ierodriguezgo@gmail.com>
2024-04-04 22:02:40 -0500

    (MP-47) Add styles for training plan section

--- src/pages/dashboard/training-plan.js
+++ src/pages/dashboard/training-plan.js
@@ -11,6 +11,7 @@ import { Seo } from 'src/components/seo';
 import { usePageView } from 'src/hooks/use-page-view';
 import { useSettings } from 'src/hooks/use-settings';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard';
+import { gray } from 'src/theme/colors';
 
 const tabs = [
   { label: 'Plan de Entrenamiento', value: 'plan-de-entrenamiento' },
@@ -49,6 +50,7 @@ const Page = () => {
           >
             <Grid
               xs={12}
+              sx={{ paddingTop: 0 }}
             >
               <Tabs
                 indicatorColor="primary"
@@ -57,6 +59,36 @@ const Page = () => {
                 textColor="primary"
                 value={currentTab}
                 variant="scrollable"
+                TabIndicatorProps={{
+                  style: { display: 'none' }
+                }}
+                sx={{
+                  '& .css-heg063-MuiTabs-flexContainer': {
+                    justifyContent: 'center',
+                    '& > .css-1uhvit7-MuiButtonBase-root-MuiTab-root:first-child': {
+                      borderRadius: '50px 0 0 50px',
+                    },
+                    '& > .css-1uhvit7-MuiButtonBase-root-MuiTab-root:last-child': {
+                      borderRadius: '0 50px 50px 0',
+                    },
+                  },
+                  '& .css-1uhvit7-MuiButtonBase-root-MuiTab-root': {
+                    width: '250px',
+                    border: `solid 1px ${gray[900]}`,
+                    backgroundColor: 'white',
+                    height: '33px',
+                    minHeight: '33px',
+                    boxShadow: `3px 3px 3px ${gray[700]}`,
+                  },
+                  '& .css-1uhvit7-MuiButtonBase-root-MuiTab-root.Mui-selected': {
+                    backgroundColor: 'primary.main',
+                    border: 'solid 1px primary.main',
+                    color: 'white'
+                  },
+                  '& .css-1uhvit7-MuiButtonBase-root-MuiTab-root+.css-1uhvit7-MuiButtonBase-root-MuiTab-root': {
+                    marginLeft: '0px'
+                  },
+                }}
               >
                 {tabs.map((tab) => (
                   <Tab
@@ -69,13 +101,16 @@ const Page = () => {
               <Divider />
             </Grid>
             {currentTab === 'plan-de-entrenamiento' && (
-              <Grid xs={12}>
+              <Grid
+                xs={12}
+                sx={{ paddingTop: '15px' }}
+              >
                 <Stack
                   direction="row"
                   justifyContent="center"
                   spacing={4}
                 >
-                  <iframe src="/assets/pdf/plan-de-entrenamiento.pdf" width="70%" height="500px" />
+                  <iframe src="/assets/pdf/plan-de-entrenamiento.pdf" width="90%" height="500px" />
                 </Stack>
                 <Box sx={{ flexGrow: 1 }}>
                 </Box>
